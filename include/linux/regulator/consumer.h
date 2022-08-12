85af901c10a3a1f11661c9ef08fe28595db15cbe
Matti Vaittinen <mazziesaccount@gmail.com>
2022-08-12 13:10:37 +0300

    regulator: Add devm helpers for get and enable
    A few regulator consumer drivers seem to be just getting a regulator,
    enabling it and registering a devm-action to disable the regulator at
    the driver detach and then forget about it.
    
    We can simplify this a bit by adding a devm-helper for this pattern.
    Add devm_regulator_get_enable() and devm_regulator_get_enable_optional()
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    Link: https://lore.kernel.org/r/ed7b8841193bb9749d426f3cb3b199c9460794cd.1660292316.git.mazziesaccount@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit da279e6965b3838e99e5c0ab8f76b87bf86b31a5)
    Stable-dep-of: 5c51d4afcf3f ("regulator: userspace-consumer: Handle regulator-output DT nodes")
    Change-Id: I36a8670be49e807dbc49987ee573436a25b8cd4f
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -189,6 +189,8 @@ struct regulator *__must_check regulator_get_optional(struct device *dev,
 						      const char *id);
 struct regulator *__must_check devm_regulator_get_optional(struct device *dev,
 							   const char *id);
+int devm_regulator_get_enable(struct device *dev, const char *id);
+int devm_regulator_get_enable_optional(struct device *dev, const char *id);
 void regulator_put(struct regulator *regulator);
 void devm_regulator_put(struct regulator *regulator);
 
@@ -231,12 +233,15 @@ int __must_check regulator_bulk_get(struct device *dev, int num_consumers,
 				    struct regulator_bulk_data *consumers);
 int __must_check devm_regulator_bulk_get(struct device *dev, int num_consumers,
 					 struct regulator_bulk_data *consumers);
+void devm_regulator_bulk_put(struct regulator_bulk_data *consumers);
 int __must_check devm_regulator_bulk_get_const(
 	struct device *dev, int num_consumers,
 	const struct regulator_bulk_data *in_consumers,
 	struct regulator_bulk_data **out_consumers);
 int __must_check regulator_bulk_enable(int num_consumers,
 				       struct regulator_bulk_data *consumers);
+int devm_regulator_bulk_get_enable(struct device *dev, int num_consumers,
+				   const char * const *id);
 int regulator_bulk_disable(int num_consumers,
 			   struct regulator_bulk_data *consumers);
 int regulator_bulk_force_disable(int num_consumers,
@@ -342,6 +347,17 @@ devm_regulator_get_exclusive(struct device *dev, const char *id)
 	return ERR_PTR(-ENODEV);
 }
 
+static inline int devm_regulator_get_enable(struct device *dev, const char *id)
+{
+	return -ENODEV;
+}
+
+static inline int devm_regulator_get_enable_optional(struct device *dev,
+						     const char *id)
+{
+	return -ENODEV;
+}
+
 static inline struct regulator *__must_check
 regulator_get_optional(struct device *dev, const char *id)
 {
@@ -360,6 +376,10 @@ static inline void regulator_put(struct regulator *regulator)
 }
 
 static inline void devm_regulator_put(struct regulator *regulator)
+{
+}
+
+static inline void devm_regulator_bulk_put(struct regulator_bulk_data *consumers)
 {
 }
 
@@ -459,6 +479,13 @@ static inline int devm_regulator_bulk_get(struct device *dev, int num_consumers,
 
 static inline int regulator_bulk_enable(int num_consumers,
 					struct regulator_bulk_data *consumers)
+{
+	return 0;
+}
+
+static inline int devm_regulator_bulk_get_enable(struct device *dev,
+						 int num_consumers,
+						 const char * const *id)
 {
 	return 0;
 }
