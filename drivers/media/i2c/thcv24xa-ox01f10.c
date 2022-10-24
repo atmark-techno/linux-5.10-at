836987fb4bbb094bdf70ab255701d32c595b46fb
Daisuke Mizobuchi <mizo@atmark-techno.com>
2022-10-25 08:34:11 +0900

    media: i2c: add error checks on probe for THCV24[12]A-P relay driver
    do not return probe success if there have been i2c errors
    
    Change-Id: Ib62aedebba9fbcc1de093685e094ec7c68cddefb
    Signed-off-by: Daisuke Mizobuchi <mizo@atmark-techno.com>

--- drivers/media/i2c/thcv24xa-ox01f10.c
+++ drivers/media/i2c/thcv24xa-ox01f10.c
@@ -26,6 +26,16 @@ struct thcv24xa_ox01f10 {
 	struct i2c_client *tx; /* THCV241A-P */
 };
 
+struct thcv24xa_ox01f10_setting {
+	struct i2c_client *client;
+	unsigned char addr_len;
+	union {
+		u8 addr8;
+		u16 addr16;
+	};
+	u8 val;
+};
+
 #define I2C_THCV241A_ADDR 0x51
 
 static int thcv24xa_ox01f10_writeb_addr16(struct i2c_client *client, u16 addr, u8 val)
@@ -79,7 +89,37 @@ static int thcv24xa_ox01f10_writeb_addr8(struct i2c_client *client, u8 addr, u8 
 	return 0;
 }
 
-static void thcv24xa_ox01f10_setup(struct thcv24xa_ox01f10 *thcv24xa_ox01f10)
+static int thcv24xa_ox01f10_writes(struct thcv24xa_ox01f10_setting *settings,
+				   int nr_settings)
+{
+	int i;
+	int ret;
+
+	for (i = 0; i < nr_settings; i++) {
+		switch (settings[i].addr_len) {
+		case 8:
+			ret = thcv24xa_ox01f10_writeb_addr8(settings[i].client,
+							    settings[i].addr8,
+							    settings[i].val);
+			if (ret)
+				return ret;
+			break;
+		case 16:
+			ret = thcv24xa_ox01f10_writeb_addr16(settings[i].client,
+							     settings[i].addr16,
+							     settings[i].val);
+			if (ret)
+				return ret;
+			break;
+		default:
+			BUG();
+		}
+	}
+
+	return 0;
+}
+
+static int thcv24xa_ox01f10_setup(struct thcv24xa_ox01f10 *thcv24xa_ox01f10)
 {
 	// [Settings for Load Function in Version 056 ]
 	//  [Welcome Screen] Execution Mode : Detail Mode
@@ -118,107 +158,109 @@ static void thcv24xa_ox01f10_setup(struct thcv24xa_ox01f10 *thcv24xa_ox01f10)
 	//  [GPIO Setting] Tx GPIO2 : No Use
 	//  [GPIO Setting] Tx GPIO3 : No Use
 	// [End of Load Settings]
+	struct thcv24xa_ox01f10_setting settings[] = {
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0050, .val = 0x51},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0004, .val = 0x03},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0010, .val = 0x10},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1704, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0102, .val = 0x02},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0103, .val = 0x02},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0104, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0105, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0100, .val = 0x03},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x010F, .val = 0x25},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x010A, .val = 0x15},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0031, .val = 0x02},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0032, .val = 0x10},
 
-	// [Rx Register Settings]
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0050, 0x51);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0004, 0x03);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0010, 0x10);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1704, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0102, 0x02);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0103, 0x02);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0104, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0105, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0100, 0x03);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x010F, 0x25);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x010A, 0x15);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0031, 0x02);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0032, 0x10);
+		// [Tx Register Setting]
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 16, .addr16 = 0x00FE, .val = 0x11},
 
-	// [Tx Register Setting]
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->tx, 0x00FE, 0x11);
+		// [Rx Register Setting only for Pass Through]
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0032, .val = 0x00},
 
-	// [Rx Register Setting only for Pass Through]
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0032, 0x00);
+		// [Tx Register Settings]
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xF3, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xF2, .val = 0x22},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xF0, .val = 0x03},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xFF, .val = 0x19},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xF6, .val = 0x15},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xC9, .val = 0x15},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xCA, .val = 0x15},
 
-	// [Tx Register Settings]
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xF3, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xF2, 0x22);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xF0, 0x03);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xFF, 0x19);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xF6, 0x15);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xC9, 0x15);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xCA, 0x15);
+		// [Tx Register Settings]
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0xFE, .val = 0x21},
 
-	// [Tx Register Settings]
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0xFE, 0x21);
+		// [PLL Settings for Pass Through mode]
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x0F, .val = 0x01},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x11, .val = 0x1E},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x12, .val = 0x4D},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x13, .val = 0x93},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x14, .val = 0x64},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x15, .val = 0x66},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x16, .val = 0x01},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x00, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x01, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x02, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x55, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x04, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x2B, .val = 0x05},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x27, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x07, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x08, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x09, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x0C, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x0D, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x2D, .val = 0x11},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x2C, .val = 0x01},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x05, .val = 0x01},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x06, .val = 0x01},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x3D, .val = 0x00},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x3E, .val = 0x10},
+		{.client = thcv24xa_ox01f10->tx, .addr_len = 8, .addr8 = 0x3F, .val = 0x01},
 
-	// [PLL Settings for Pass Through mode]
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x0F, 0x01);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x11, 0x1E);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x12, 0x4D);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x13, 0x93);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x14, 0x64);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x15, 0x66);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x16, 0x01);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x00, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x01, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x02, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x55, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x04, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x2B, 0x05);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x27, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x07, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x08, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x09, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x0C, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x0D, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x2D, 0x11);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x2C, 0x01);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x05, 0x01);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x06, 0x01);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x3D, 0x00);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x3E, 0x10);
-	thcv24xa_ox01f10_writeb_addr8(thcv24xa_ox01f10->tx, 0x3F, 0x01);
+		// [ Rx Register Settings]
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0010, .val = 0x11},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1010, .val = 0xA1},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1011, .val = 0x05},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1012, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1021, .val = 0x30},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1022, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1023, .val = 0x31},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1024, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1025, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1026, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1027, .val = 0x07},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1028, .val = 0x02},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1030, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1100, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1101, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1102, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1600, .val = 0x1A},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1605, .val = 0x29},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1606, .val = 0x44},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x161F, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1609, .val = 0x04},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x160A, .val = 0x07},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x160B, .val = 0x05},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x160D, .val = 0x08},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x160E, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x160F, .val = 0x03},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1610, .val = 0x02},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1611, .val = 0x03},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1612, .val = 0x06},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1013, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1703, .val = 0x01},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1704, .val = 0x11},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1003, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x1004, .val = 0x00},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x001B, .val = 0x18},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0032, .val = 0x10},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0040, .val = 0x36},
+		{.client = thcv24xa_ox01f10->rx, .addr_len = 16, .addr16 = 0x0041, .val = 0x36},
+	};
 
-	// [ Rx Register Settings]
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0010, 0x11);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1010, 0xA1);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1011, 0x05);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1012, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1021, 0x30);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1022, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1023, 0x31);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1024, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1025, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1026, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1027, 0x07);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1028, 0x02);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1030, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1100, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1101, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1102, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1600, 0x1A);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1605, 0x29);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1606, 0x44);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x161F, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1609, 0x04);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x160A, 0x07);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x160B, 0x05);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x160D, 0x08);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x160E, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x160F, 0x03);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1610, 0x02);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1611, 0x03);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1612, 0x06);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1013, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1703, 0x01);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1704, 0x11);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1003, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x1004, 0x00);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x001B, 0x18);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0032, 0x10);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0040, 0x36);
-	thcv24xa_ox01f10_writeb_addr16(thcv24xa_ox01f10->rx, 0x0041, 0x36);
+	return thcv24xa_ox01f10_writes(settings, ARRAY_SIZE(settings));
 }
 
 static int thcv24xa_ox01f10_probe(struct i2c_client *client,
@@ -253,9 +295,7 @@ static int thcv24xa_ox01f10_probe(struct i2c_client *client,
 	 */
 	msleep(200);
 
-	thcv24xa_ox01f10_setup(thcv24xa_ox01f10);
-
-	return 0;
+	return thcv24xa_ox01f10_setup(thcv24xa_ox01f10);
 }
 
 static struct i2c_driver thcv24xa_ox01f10_driver = {
