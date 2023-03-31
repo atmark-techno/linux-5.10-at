dd3b6f21c244a4e9f3731a08a3006e2fabe242fe
Dominique Martinet <dominique.martinet@atmark-techno.com>
2023-03-31 16:04:09 +0900

    net/wireless/nxp: revert nxp wifi driver to upstream state
    revert drivers/net/wireless/nxp back to original commit 5dfc6a7c4e0e
    ("net/wireless/nxp: update moal driver to
    17.68.1.p136.19-17.26.1.p136.19-MXM5X17366_P7_V0V1") state.
    
    This will be useful to re-apply our changes after the driver update.
    
    Done with:
    git checkout 5dfc6a7c4e0e -- drivers/net/wireless/nxp
    
    Change-Id: I3d11c990bbdc44c8ea1c2c60455cf1fb429664ca
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/net/wireless/nxp/mlinux/moal_pcie.c
+++ drivers/net/wireless/nxp/mlinux/moal_pcie.c
@@ -442,7 +442,6 @@ err_init_fw:
 static int woal_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	pcie_service_card *card = NULL;
-	moal_handle *handle;
 	t_u16 card_type = 0;
 	int ret = 0;
 
@@ -479,11 +478,11 @@ static int woal_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		goto err;
 	}
 
-	handle = woal_add_card(card, &card->dev->dev, &pcie_ops, card_type);
-	if (handle == NULL || IS_ERR(handle)) {
+	if (woal_add_card(card, &card->dev->dev, &pcie_ops, card_type) ==
+	    NULL) {
 		woal_pcie_cleanup(card);
-		PRINTM(MMSG, "%s: failed\n", __func__);
-		ret = IS_ERR(handle) ? PTR_ERR(handle) : -EFAULT;
+		PRINTM(MERROR, "%s: failed\n", __func__);
+		ret = -EFAULT;
 		goto err;
 	}
 
