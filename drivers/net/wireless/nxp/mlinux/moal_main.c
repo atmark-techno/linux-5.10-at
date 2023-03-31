0dce1f9c3e59868debb9eedc977c2549129e5d1f
Dominique Martinet <dominique.martinet@atmark-techno.com>
2023-03-31 16:08:04 +0900

    Revert "net/wireless/nxp: revert nxp wifi driver to upstream state"
    This reverts the previous reset to upstream commit and thus restores our
    custom patches.
    
    Change-Id: I86d13e1e177f8aad7bf9b362d5b401c45e69365d
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/net/wireless/nxp/mlinux/moal_main.c
+++ drivers/net/wireless/nxp/mlinux/moal_main.c
@@ -2798,7 +2798,7 @@ static mlan_status woal_add_card_dpc(moal_handle *handle)
 		}
 	}
 	woal_get_version(handle, str_buf, sizeof(str_buf) - 1);
-	PRINTM(MMSG, "wlan: version = %s\n", str_buf);
+	pr_info("wlan: version = %s\n", str_buf);
 
 	handle->woal_notifier.notifier_call = woal_netdevice_event;
 	if (register_inetaddr_notifier(&handle->woal_notifier)) {
@@ -9623,6 +9623,7 @@ moal_handle *woal_add_card(void *card, struct device *dev, moal_if_ops *if_ops,
 		.groups = NL_MULTICAST_GROUP,
 	};
 #endif
+	int ret = 0;
 
 	ENTER();
 
@@ -9673,6 +9674,7 @@ moal_handle *woal_add_card(void *card, struct device *dev, moal_if_ops *if_ops,
 	if (!handle->params.drv_mode) {
 		PRINTM(MMSG, "wlan: stop init_adapter, drv_mode=%d\n",
 		       handle->params.drv_mode);
+		ret = -ENODEV;
 		goto err_kmalloc;
 	}
 #ifdef IMX_SUPPORT
@@ -9923,6 +9925,8 @@ err_handle:
 	MOAL_REL_SEMAPHORE(&AddRemoveCardSem);
 exit_sem_err:
 	LEAVE();
+	if (ret)
+		return ERR_PTR(ret);
 	return NULL;
 }
 
--- drivers/net/wireless/nxp/mlinux/moal_pcie.c
+++ drivers/net/wireless/nxp/mlinux/moal_pcie.c
@@ -442,6 +442,7 @@ err_init_fw:
 static int woal_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	pcie_service_card *card = NULL;
+	moal_handle *handle;
 	t_u16 card_type = 0;
 	int ret = 0;
 
@@ -478,11 +479,11 @@ static int woal_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		goto err;
 	}
 
-	if (woal_add_card(card, &card->dev->dev, &pcie_ops, card_type) ==
-	    NULL) {
+	handle = woal_add_card(card, &card->dev->dev, &pcie_ops, card_type);
+	if (handle == NULL || IS_ERR(handle)) {
 		woal_pcie_cleanup(card);
-		PRINTM(MERROR, "%s: failed\n", __func__);
-		ret = -EFAULT;
+		PRINTM(MMSG, "%s: failed\n", __func__);
+		ret = IS_ERR(handle) ? PTR_ERR(handle) : -EFAULT;
 		goto err;
 	}
 
