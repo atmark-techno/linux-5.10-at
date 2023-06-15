348359ab5ea17b5a4e190cec1da734ede72844b7
Alice Yuan <alice.yuan@nxp.com>
2023-06-15 19:00:57 +0800

    LF-9127-3: media: isi: support suspend/resume when camera is running
    When system enter suspend mode, the capture pipeline SENSOR->CSI->ISI
    can't work normally after resume, because the ISI power supplies are
    lost. So if we want to resume normally, the ISI's clock should be
    re-enabled.The isi also should be reset and re-configured.
    
    Meanwhile the pipeline should follow the correct order.
    Suspend order: SENSOR->CSI->ISI, resume order: ISI->CSI->SENSOR
    
    Change-Id: I9ddc3c691649896c438296b52f47156a81c5038a
    Signed-off-by: Alice Yuan <alice.yuan@nxp.com>
    Reviewed-by: G.N. Zhou <guoniu.zhou@nxp.com>
    (cherry picked from commit a0574a3c217e92cae43ceec34d5ddf8e57efe083)
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/staging/media/imx/imx8-isi-core.c
+++ drivers/staging/media/imx/imx8-isi-core.c
@@ -92,7 +92,7 @@ static int disp_mix_sft_parse_resets(struct mxc_isi_dev *mxc_isi)
 	return pdata->rst_ops->parse(mxc_isi);
 }
 
-static int disp_mix_sft_rstn(struct mxc_isi_dev *mxc_isi, bool enable)
+int disp_mix_sft_rstn(struct mxc_isi_dev *mxc_isi, bool enable)
 {
 	struct mxc_isi_plat_data const *pdata = mxc_isi->pdata;
 	int ret;
@@ -109,6 +109,7 @@ static int disp_mix_sft_rstn(struct mxc_isi_dev *mxc_isi, bool enable)
 		       pdata->rst_ops->deassert(mxc_isi);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(disp_mix_sft_rstn);
 
 static int disp_mix_clks_get(struct mxc_isi_dev *mxc_isi)
 {
@@ -123,7 +124,7 @@ static int disp_mix_clks_get(struct mxc_isi_dev *mxc_isi)
 	return pdata->gclk_ops->gclk_get(mxc_isi);
 }
 
-static int disp_mix_clks_enable(struct mxc_isi_dev *mxc_isi, bool enable)
+int disp_mix_clks_enable(struct mxc_isi_dev *mxc_isi, bool enable)
 {
 	struct mxc_isi_plat_data const *pdata = mxc_isi->pdata;
 	int ret;
@@ -140,6 +141,7 @@ static int disp_mix_clks_enable(struct mxc_isi_dev *mxc_isi, bool enable)
 		       pdata->gclk_ops->gclk_disable(mxc_isi);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(disp_mix_clks_enable);
 
 static int mxc_imx8_clk_get(struct mxc_isi_dev *mxc_isi)
 {
@@ -626,7 +628,7 @@ static int mxc_isi_clk_get(struct mxc_isi_dev *mxc_isi)
 	return ops->clk_get(mxc_isi);
 }
 
-static int mxc_isi_clk_enable(struct mxc_isi_dev *mxc_isi)
+int mxc_isi_clk_enable(struct mxc_isi_dev *mxc_isi)
 {
 	const struct mxc_isi_dev_ops *ops = mxc_isi->pdata->ops;
 
@@ -635,8 +637,9 @@ static int mxc_isi_clk_enable(struct mxc_isi_dev *mxc_isi)
 
 	return ops->clk_enable(mxc_isi);
 }
+EXPORT_SYMBOL_GPL(mxc_isi_clk_enable);
 
-static void mxc_isi_clk_disable(struct mxc_isi_dev *mxc_isi)
+void mxc_isi_clk_disable(struct mxc_isi_dev *mxc_isi)
 {
 	const struct mxc_isi_dev_ops *ops = mxc_isi->pdata->ops;
 
@@ -645,6 +648,7 @@ static void mxc_isi_clk_disable(struct mxc_isi_dev *mxc_isi)
 
 	ops->clk_disable(mxc_isi);
 }
+EXPORT_SYMBOL_GPL(mxc_isi_clk_disable);
 
 static int mxc_isi_soc_match(struct mxc_isi_dev *mxc_isi,
 			     const struct soc_device_attribute *data)
@@ -683,7 +687,6 @@ static int mxc_isi_probe(struct platform_device *pdev)
 	struct resource *res;
 	const struct of_device_id *of_id;
 	int ret = 0;
-
 
 	mxc_isi = devm_kzalloc(dev, sizeof(*mxc_isi), GFP_KERNEL);
 	if (!mxc_isi)
@@ -810,13 +813,6 @@ static int mxc_isi_remove(struct platform_device *pdev)
 
 static int mxc_isi_pm_suspend(struct device *dev)
 {
-	struct mxc_isi_dev *mxc_isi = dev_get_drvdata(dev);
-
-	if (mxc_isi->is_streaming) {
-		dev_warn(dev, "running, prevent entering suspend.\n");
-		return -EAGAIN;
-	}
-
 	return pm_runtime_force_suspend(dev);
 }
 
@@ -827,32 +823,16 @@ static int mxc_isi_pm_resume(struct device *dev)
 
 static int mxc_isi_runtime_suspend(struct device *dev)
 {
-	struct mxc_isi_dev *mxc_isi = dev_get_drvdata(dev);
-
-	disp_mix_clks_enable(mxc_isi, false);
-	mxc_isi_clk_disable(mxc_isi);
-
 	return 0;
 }
 
 static int mxc_isi_runtime_resume(struct device *dev)
 {
-	struct mxc_isi_dev *mxc_isi = dev_get_drvdata(dev);
-	int ret;
-
-	ret = mxc_isi_clk_enable(mxc_isi);
-	if (ret) {
-		dev_err(dev, "%s clk enable fail\n", __func__);
-		return ret;
-	}
-	disp_mix_sft_rstn(mxc_isi, false);
-	disp_mix_clks_enable(mxc_isi, true);
-
 	return 0;
 }
 
 static const struct dev_pm_ops mxc_isi_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(mxc_isi_pm_suspend, mxc_isi_pm_resume)
+	SET_LATE_SYSTEM_SLEEP_PM_OPS(mxc_isi_pm_suspend, mxc_isi_pm_resume)
 	SET_RUNTIME_PM_OPS(mxc_isi_runtime_suspend, mxc_isi_runtime_resume, NULL)
 };
 
