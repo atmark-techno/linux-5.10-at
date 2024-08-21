f0b7c77a5dd37348db818ac14719d5ee92d364d5
Daisuke Mizobuchi <mizo@atmark-techno.com>
2024-08-21 16:26:14 +0900

    serial: imx_rpmsg: add the rpmsg tty driver
    This driver works with atmark-techno m33 firmware. It passes through
    UART.
    
    The only UART setting that can be changed is the baud rate.
    
    Change-Id: I45f94f95d2cdfb989c926f292bd5c3d5862ae6d4

--- include/linux/imx_rpmsg.h
+++ include/linux/imx_rpmsg.h
@@ -28,6 +28,7 @@
 #define IMX_RPMSG_I2C		9
 #define IMX_RPMSG_PWM		0xa
 #define IMX_RPMSG_ADC		0xb
+#define IMX_RPMSG_TTY		0xf1
 /* rpmsg version */
 #define IMX_RMPSG_MAJOR		1
 #define IMX_RMPSG_MINOR		0
