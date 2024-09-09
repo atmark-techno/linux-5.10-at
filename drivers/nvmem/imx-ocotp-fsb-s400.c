ea3ec05914cdc5e73f42a9b5bd2afcf9e1d34b66
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-09-09 16:26:18 +0900

    firmware/imx/ele & nvmem/imx-ocotp-fsb-s400: backport from lf-6.6.23-2.0.0
    the nvmem imx-ocotp-fsb-s400 driver would read junk for some fields
    (two reads in a row):
    armadillo:~# xxd < /sys/bus/nvmem/devices/fsb_s400_fuse0/nvmem  | grep -A 1 640
    00000640: 79ff ff70 b268 ec11 79ff ff70 b268 ec11  y..p.h..y..p.h..
    00000650: 79ff ff00 b068 ec11 79ff ff00 0000 0000  y....h..y.......
    armadillo:~# xxd < /sys/bus/nvmem/devices/fsb_s400_fuse0/nvmem  | grep -A 1 640
    00000640: 0000 0000 0000 0000 0000 0000 0000 0000  ................
    00000650: 0000 0000 0000 0000 0000 0000 0000 0000  ................
    
    Updating this communication layer fixes it.
    
    The version of the ele files first added in the 6.6 tree was already
    wildly different from what we have, so instead of fighting with
    cherry-picks copy files directly from nxp's lf-6.6.23-2.0.0 and
    adjust as follow.
    
    drivers/firmware/imx:
    - FW_ACTION_UEVENT does not exist in 5.10, renaming to FW_ACTION_HOTPLUG
      is enough for us
    - file_open_root: arguments changed slightly, fix to work
    - CONFIG_IMX_ELE_TRNG default was changed to n (we don't use it)
    - Kconfig: makei IMX_SEC_ENCLAVE default y; it doesn't work with m
    
    nvmem/imx-ocotp-fsb-s400:
    - framework to fix imx93's mac address are not implemented in this
      kernel (read_post_process, cell->priv, eth_addr_add), just remove it
      all as we only care about 8ulp
    
    imx8ulp.dtsi:
    - fix ele nodes as appropriate. Note the part in imx8mp.dtsi is also
      required, so next commit fixes armadillo_900.dtsi
    
    caam:
    - replace fw node as per 6.6
    
    gpio:
    - remove duplicate define (identical to updated rsrc.h)
    
    mailbox:
    - add function definition in .h as per upstream, that was not
    part of a different commit there
    
    Change-Id: I4a04301af5fcf731ec65d503a1b1cf0f8be78966
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/nvmem/imx-ocotp-fsb-s400.c
+++ drivers/nvmem/imx-ocotp-fsb-s400.c
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright 2021 NXP
- * Author: Alice Guo <alice.guo@nxp.com>
  */
 
+#include <linux/of_address.h>
 #include <linux/dev_printk.h>
 #include <linux/errno.h>
-#include <linux/firmware/imx/sentnl_base_msg.h>
+#include <linux/firmware/imx/ele_base_msg.h>
+#include <linux/firmware/imx/se_fw_inc.h>
 #include <linux/io.h>
+#include <linux/etherdevice.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
@@ -20,8 +22,15 @@
 #define LOCK_CFG	0x01
 #define ECID		0x02
 #define UNIQ_ID		0x07
-#define SRK_HASH	0x0F
 #define OTFAD_CFG	0x17
+#define MAPPING_SIZE	0x20
+#define FUSE_ACC_DIS	0x28
+
+enum soc_type {
+	IMX8ULP,
+	IMX93,
+	IMX95,
+};
 
 struct bank_2_reg {
 	unsigned int bank;
@@ -29,63 +38,54 @@ struct bank_2_reg {
 	bool flag;
 };
 
-static const struct bank_2_reg fsb_bank_reg[] = {
-	{ 3, 0 },
-	{ 4, 8 },
-	{ 5, 64 },
-	{ 6, 72 },
-	{ 8, 80, true },
-	{ 24, 84, true },
-	{ 26, 88, true },
-	{ 27, 92, true },
-	{ 28, 96 },
-	{ 29, 104 },
-	{ 30, 112 },
-	{ 31, 120 },
-	{ 37, 128 },
-	{ 38, 136 },
-	{ 39, 144 },
-	{ 40, 152 },
-	{ 41, 160 },
-	{ 42, 168 },
-	{ 43, 176 },
-	{ 44, 184 },
-	{ 45, 192 },
-	{ 46, 200 },
+struct imx_fsb_s400_hw {
+	enum soc_type soc;
+	unsigned int fsb_otp_shadow;
+	const uint8_t se_pdev_name[20];
+	const struct bank_2_reg fsb_bank_reg[MAPPING_SIZE];
+	bool oscca_fuse_read;
+	const u8 *pf_mac_offset_list;
 };
 
 struct imx_fsb_s400_fuse {
 	void __iomem *regs;
 	struct nvmem_config config;
 	struct mutex lock;
+	struct device *se_dev;
+	const struct imx_fsb_s400_hw *hw;
+	bool fsb_read_dis;
+	u8 pfn;
 };
 
-static int read_words_via_s400_api(u32 *buf, unsigned int fuse_base, unsigned int num)
+static int read_words_via_s400_api(u32 *buf, unsigned int fuse_base,
+				   unsigned int num, struct device *se_dev)
 {
 	unsigned int i;
 	int err = 0;
 
 	for (i = 0; i < num; i++)
-		err = read_common_fuse(fuse_base + i, buf + i);
+		err = read_common_fuse(se_dev, fuse_base + i, buf + i);
 
 	return err;
 }
 
-static int read_words_via_fsb(void __iomem *regs, unsigned int bank, u32 *buf)
+static int read_words_via_fsb(void *priv, unsigned int bank, u32 *buf)
 {
+	struct imx_fsb_s400_fuse *fuse = priv;
+	void __iomem *regs = fuse->regs + fuse->hw->fsb_otp_shadow;
 	unsigned int i;
 	unsigned int reg_id = UINT_MAX;
-	unsigned int size = ARRAY_SIZE(fsb_bank_reg);
+	unsigned int size = ARRAY_SIZE(fuse->hw->fsb_bank_reg);
 
 	for (i = 0; i < size; i++) {
-		if (fsb_bank_reg[i].bank == bank) {
-			reg_id = fsb_bank_reg[i].reg;
+		if (fuse->hw->fsb_bank_reg[i].bank == bank) {
+			reg_id = fuse->hw->fsb_bank_reg[i].reg;
 			break;
 		}
 	}
 
 	if (reg_id != UINT_MAX) {
-		size = fsb_bank_reg[i].flag ? 4 : 8;
+		size = fuse->hw->fsb_bank_reg[i].flag ? 4 : 8;
 
 		for (i = 0; i < size; i++) {
 			*buf = readl_relaxed(regs + (reg_id + i) * 4);
@@ -96,13 +96,26 @@ static int read_words_via_fsb(void __iomem *regs, unsigned int bank, u32 *buf)
 	return 0;
 }
 
+static int read_nwords_via_fsb(void __iomem *regs, u32 *buf, u32 fuse_base, u32 num)
+{
+	unsigned int i;
+
+	for (i = 0; i < num; i++) {
+		*buf = readl_relaxed(regs + (fuse_base + i) * 4);
+		buf = buf + 1;
+	}
+
+	return 0;
+}
+
 static int fsb_s400_fuse_read(void *priv, unsigned int offset, void *val,
 			      size_t bytes)
 {
 	struct imx_fsb_s400_fuse *fuse = priv;
+	void __iomem *regs = fuse->regs + fuse->hw->fsb_otp_shadow;
 	unsigned int num_bytes, bank;
 	u32 *buf;
-	int err;
+	int err, i;
 
 	num_bytes = round_up(2048, 4);
 	buf = kzalloc(num_bytes, GFP_KERNEL);
@@ -112,68 +125,134 @@ static int fsb_s400_fuse_read(void *priv, unsigned int offset, void *val,
 	err = -EINVAL;
 
 	mutex_lock(&fuse->lock);
-	for (bank = 0; bank < 63; bank++) {
-		switch (bank) {
-		case 0:
-			break;
-		case LOCK_CFG:
-			err = read_words_via_s400_api(&buf[LOCK_CFG*8], 8, 8);
-			if (err)
-				goto ret;
-			break;
-		case ECID:
-			err = read_words_via_s400_api(&buf[ECID*8], 16, 8);
-			if (err)
-				goto ret;
-			break;
-		case UNIQ_ID:
-			err = read_common_fuse(OTP_UNIQ_ID, &buf[UNIQ_ID*8]);
-			if (err)
-				goto ret;
-			break;
-		case SRK_HASH:
-			err = read_common_fuse(OTP_SRK_HASH, &buf[SRK_HASH*8]);
-			if (err)
-				goto ret;
-			break;
-		case OTFAD_CFG:
-			err = read_common_fuse(OTFAD_CONFIG, &buf[OTFAD_CFG*8]);
-			if (err)
-				goto ret;
-			break;
-		case 25:
-		case 26:
-			break; // read @27
-		case 27:
-			err = read_words_via_s400_api(&buf[200], 200, 24);
-			if (err)
-				goto ret;
-			break;
-		case 32:
-		case 33:
-		case 34:
-		case 35:
-			break; // read @36
-		case 36:
-			err = read_words_via_s400_api(&buf[256], 256, 40);
-			if (err)
-				goto ret;
-			break;
-		case 49:
-		case 50:
-			break; // read @51
-		case 51:
-			err = read_words_via_s400_api(&buf[392], 392, 24);
-			if (err)
-				goto ret;
-			break;
-		default:
-			err = read_words_via_fsb(fuse->regs + 0x800, bank, &buf[bank * 8]);
-			break;
+	if (fuse->hw->soc == IMX8ULP) {
+		for (bank = 0; bank < 63; bank++) {
+			switch (bank) {
+			case 0:
+				break;
+			case LOCK_CFG:
+				err = read_words_via_s400_api(&buf[8], 8, 8,
+							      fuse->se_dev);
+				if (err)
+					goto ret;
+				break;
+			case ECID:
+				err = read_words_via_s400_api(&buf[16], 16, 8,
+							      fuse->se_dev);
+				if (err)
+					goto ret;
+				break;
+			case UNIQ_ID:
+				err = read_common_fuse(fuse->se_dev,
+						       OTP_UNIQ_ID,
+						       &buf[56]);
+				if (err)
+					goto ret;
+				break;
+			case OTFAD_CFG:
+				err = read_common_fuse(fuse->se_dev,
+						       OTFAD_CONFIG, &buf[184]);
+				if (err)
+					goto ret;
+				break;
+			case 25:
+			case 26:
+			case 27:
+				err = read_words_via_s400_api(&buf[200], 200, 24,
+							      fuse->se_dev);
+				if (err)
+					goto ret;
+				break;
+			case 32:
+			case 33:
+			case 34:
+			case 35:
+			case 36:
+				err = read_words_via_s400_api(&buf[256], 256, 40,
+							      fuse->se_dev);
+				if (err)
+					goto ret;
+				break;
+			case 49:
+			case 50:
+			case 51:
+				err = read_words_via_s400_api(&buf[392], 392, 24,
+							      fuse->se_dev);
+				if (err)
+					goto ret;
+				break;
+			default:
+				err = read_words_via_fsb(priv, bank, &buf[bank * 8]);
+				break;
+			}
 		}
+	} else if (fuse->hw->soc == IMX93) {
+		for (bank = 0; bank < 6; bank++) {
+			if (fuse->fsb_read_dis)
+				read_words_via_s400_api(&buf[bank * 8], bank * 8,
+							8, fuse->se_dev);
+			else
+				read_nwords_via_fsb(regs, &buf[bank * 8], bank * 8, 8);
+		}
+
+		if (fuse->fsb_read_dis)
+			read_words_via_s400_api(&buf[48], 48, 4, fuse->se_dev);
+		else
+			read_nwords_via_fsb(regs, &buf[48], 48, 4); /* OTP_UNIQ_ID */
+
+		err = read_words_via_s400_api(&buf[63], 63, 1, fuse->se_dev);
+		if (err)
+			goto ret;
+
+		err = read_words_via_s400_api(&buf[128], 128, 16, fuse->se_dev);
+		if (err)
+			goto ret;
+
+		err = read_words_via_s400_api(&buf[182], 182, 1, fuse->se_dev);
+		if (err)
+			goto ret;
+
+		err = read_words_via_s400_api(&buf[188], 188, 1, fuse->se_dev);
+		if (err)
+			goto ret;
+
+		for (bank = 39; bank < 64; bank++) {
+			if (fuse->fsb_read_dis)
+				read_words_via_s400_api(&buf[bank * 8], bank * 8,
+					       		8, fuse->se_dev);
+			else
+				read_nwords_via_fsb(regs, &buf[bank * 8], bank * 8, 8);
+		}
+	} else if (fuse->hw->soc == IMX95) {
+		buf[0] = readl_relaxed(regs + 0 * 4) & 0xffff;
+		buf[7] = readl_relaxed(regs + 7 * 4) & 0xffff;
+		buf[9] = readl_relaxed(regs + 9 * 4) & 0xffff;
+		buf[10] = readl_relaxed(regs + 10 * 4) & 0xffff;
+		buf[11] = readl_relaxed(regs + 11 * 4) & 0xffff;
+		for (i = 12; i < 36; i++)
+			buf[i] = readl_relaxed(regs + i * 4);
+		buf[36] = readl_relaxed(regs + 36 * 4) & 0xffff;
+		buf[37] = readl_relaxed(regs + 37 * 4) & 0xffff;
+		for (i = 38; i < 52; i++)
+			buf[i] = readl_relaxed(regs + i * 4);
+		buf[317] = readl_relaxed(regs + 317 * 4) & 0xffff;
+		buf[318] = readl_relaxed(regs + 318 * 4) & 0xffff;
+		for (i = 320; i < 327; i++)
+			buf[i] = readl_relaxed(regs + i * 4);
+		for (i = 328; i < 512; i++)
+			buf[i] = readl_relaxed(regs + i * 4);
+
+		read_words_via_s400_api(&buf[63], 63, 1, fuse->se_dev);
+		read_words_via_s400_api(&buf[128], 128, 16, fuse->se_dev);
+		read_words_via_s400_api(&buf[188], 188, 1, fuse->se_dev);
+
+		err = 0;
+
+		fuse->pfn = offset >> 12 & 0xf;
+		offset = offset & 0xfff;
 	}
 
-	memcpy(val, (u8 *)(buf + offset), bytes);
+	memcpy(val, (u8 *)(buf) + offset, bytes);
 
 ret:
 	kfree(buf);
@@ -182,10 +261,33 @@ ret:
 	return err;
 }
 
+static int fsb_s400_fuse_write(void *priv, unsigned int offset, void *val, size_t bytes)
+{
+	struct imx_fsb_s400_fuse *fuse = priv;
+	u32 *buf = val;
+	u32 index;
+	int ret;
+
+	/* allow only writing one complete OTP word at a time */
+	if (bytes != 4)
+		return -EINVAL;
+
+	index = offset;
+
+	mutex_lock(&fuse->lock);
+	ret = ele_write_fuse(fuse->se_dev, index, *buf, false);
+	mutex_unlock(&fuse->lock);
+
+	return ret;
+}
+
 static int imx_fsb_s400_fuse_probe(struct platform_device *pdev)
 {
 	struct imx_fsb_s400_fuse *fuse;
 	struct nvmem_device *nvmem;
+	struct device_node *np;
+	void __iomem *reg;
+	u32 v;
 
 	fuse = devm_kzalloc(&pdev->dev, sizeof(*fuse), GFP_KERNEL);
 	if (!fuse)
@@ -201,7 +303,30 @@ static int imx_fsb_s400_fuse_probe(struct platform_device *pdev)
 	fuse->config.owner = THIS_MODULE;
 	fuse->config.size = 2048; /* 64 Banks */
 	fuse->config.reg_read = fsb_s400_fuse_read;
+	if ((of_device_is_compatible(pdev->dev.of_node, "fsl,imx93-ocotp")) ||
+	    (of_device_is_compatible(pdev->dev.of_node, "fsl,imx95-ocotp")))
+		fuse->config.reg_write = fsb_s400_fuse_write;
 	fuse->config.priv = fuse;
+	mutex_init(&fuse->lock);
+	fuse->hw = of_device_get_match_data(&pdev->dev);
+
+	if (fuse->hw->oscca_fuse_read) {
+		np = of_find_compatible_node(NULL, NULL, "fsl,imx93-aonmix-ns-syscfg");
+		if (!np)
+			return -ENODEV;
+
+		reg = of_iomap(np, 0);
+		if (!reg)
+			return -ENOMEM;
+
+		v = readl_relaxed(reg + FUSE_ACC_DIS);
+		if (v & BIT(0))
+			fuse->fsb_read_dis = true;
+		else
+			fuse->fsb_read_dis = false;
+	} else {
+		fuse->fsb_read_dis = false;
+	}
 
 	nvmem = devm_nvmem_register(&pdev->dev, &fuse->config);
 	if (IS_ERR(nvmem)) {
@@ -209,15 +334,53 @@ static int imx_fsb_s400_fuse_probe(struct platform_device *pdev)
 		return PTR_ERR(nvmem);
 	}
 
-	mutex_init(&fuse->lock);
-
+	fuse->se_dev = get_se_dev(fuse->hw->se_pdev_name);
+	if (!fuse->se_dev) {
+		dev_err(&pdev->dev, "No se device\n");
+		return -ENODEV;
+	}
 	dev_dbg(&pdev->dev, "fuse nvmem device registered successfully\n");
 
 	return 0;
 }
 
+static const u8 imx95_pf_mac_offset_list[] = { 0, 3, 6 };
+
+static const struct imx_fsb_s400_hw imx8ulp_fsb_s400_hw = {
+	.soc = IMX8ULP,
+	.fsb_otp_shadow = 0x800,
+	.fsb_bank_reg = {
+		[0] = { 3, 0 },
+		[1] = { 4, 8 },
+		[2] = { 5, 64 },
+		[3] = { 6, 72 },
+		[4] = { 8, 80, true },
+		[5] = { 24, 84, true },
+		[6] = { 26, 88, true },
+		[7] = { 27, 92, true },
+		[8] = { 28, 96 },
+		[9] = { 29, 104 },
+		[10] = { 30, 112 },
+		[11] = { 31, 120 },
+		[12] = { 37, 128 },
+		[13] = { 38, 136 },
+		[14] = { 39, 144 },
+		[15] = { 40, 152 },
+		[16] = { 41, 160 },
+		[17] = { 42, 168 },
+		[18] = { 43, 176 },
+		[19] = { 44, 184 },
+		[20] = { 45, 192 },
+		[21] = { 46, 200 },
+	},
+	.oscca_fuse_read = false,
+	.pf_mac_offset_list = NULL,
+	.se_pdev_name = "se-fw2",
+};
+
 static const struct of_device_id imx_fsb_s400_fuse_match[] = {
-	{ .compatible = "fsl,imx8ulp-ocotp", },
+	{ .compatible = "fsl,imx8ulp-ocotp", .data = &imx8ulp_fsb_s400_hw, },
+	// imx93 has been removed because post process hook not supported in this kernel
 	{},
 };
 
@@ -228,8 +391,9 @@ static struct platform_driver imx_fsb_s400_fuse_driver = {
 	},
 	.probe = imx_fsb_s400_fuse_probe,
 };
+MODULE_DEVICE_TABLE(of, imx_fsb_s400_fuse_match);
 module_platform_driver(imx_fsb_s400_fuse_driver);
 
-MODULE_AUTHOR("Alice Guo <alice.guo@nxp.com>");
+MODULE_AUTHOR("NXP");
 MODULE_DESCRIPTION("i.MX FSB/S400-API ocotp fuse box driver");
 MODULE_LICENSE("GPL v2");
