5cce10ba1fdf08dc206469f3ec764fc3199579a5
Dominique Martinet <dominique.martinet@atmark-techno.com>
2025-11-21 15:15:01 +0900

    usb: phy: mxs: fix re-connecting line on vbus enable from chipidea
    upstream commit 2b6794a5eca7 ("usb: chipidea: add USB PHY event") was
    backported into 5.10 and broke reconnecting vbus:
    
    armadillo:~# ls -l /dev/video*
    crw-rw----    1 root     video      81,   0 Jan  1  1970 /dev/video0
    crw-rw----    1 root     video      81,   1 Jan  1  1970 /dev/video1
    armadillo:~# echo disabled > /sys/devices/platform/usb1-vbus/state
    armadillo:~# ls -l /dev/video*
    ls: /dev/video*: No such file or directory
    armadillo:~# echo enabled > /sys/devices/platform/usb1-vbus/state
    armadillo:~# ls -l /dev/video*
    crw-rw----    1 root     video      81,   0 Nov 18 17:39 /dev/video0
    crw-rw----    1 root     video      81,   1 Nov 18 17:39 /dev/video1
    
    This fixes that problem by no longer considering mode (no longer
    updated) but using last_event as per upstream (code was picked from the
    6.12 branch)
    
    Link: https://armadillo.atmark-techno.com/forum/armadillo/28497
    Change-Id: I67baa78f4d65039964221a333a09735cdc047586
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/usb/phy/phy-mxs-usb.c
+++ drivers/usb/phy/phy-mxs-usb.c
@@ -274,7 +274,6 @@ struct mxs_phy {
 	u32 tx_reg_mask;
 	struct regulator *phy_3p0;
 	bool hardware_control_phy2_clk;
-	enum usb_current_mode mode;
 	unsigned long clk_rate;
 };
 
@@ -473,6 +472,11 @@ static void __mxs_phy_disconnect_line(struct mxs_phy *mxs_phy, bool disconnect)
 		usleep_range(500, 1000);
 }
 
+static bool mxs_phy_is_otg_host(struct mxs_phy *mxs_phy)
+{
+	return mxs_phy->phy.last_event == USB_EVENT_ID;
+}
+
 static void mxs_phy_disconnect_line(struct mxs_phy *mxs_phy, bool on)
 {
 	bool vbus_is_on = false;
@@ -488,8 +492,8 @@ static void mxs_phy_disconnect_line(struct mxs_phy *mxs_phy, bool on)
 
 	vbus_is_on = mxs_phy_get_vbus_status(mxs_phy);
 
-	if (on && ((!vbus_is_on && mxs_phy->mode != CUR_USB_MODE_HOST) ||
-			(last_event == USB_EVENT_VBUS)))
+	if (on && ((!vbus_is_on && !mxs_phy_is_otg_host(mxs_phy))
+		|| (last_event == USB_EVENT_VBUS)))
 		__mxs_phy_disconnect_line(mxs_phy, true);
 	else
 		__mxs_phy_disconnect_line(mxs_phy, false);
@@ -867,19 +871,6 @@ static int mxs_phy_on_resume(struct usb_phy *phy,
 	return 0;
 }
 
-/*
- * Set the usb current role for phy.
- */
-static int mxs_phy_set_mode(struct usb_phy *phy,
-		enum usb_current_mode mode)
-{
-	struct mxs_phy *mxs_phy = to_mxs_phy(phy);
-
-	mxs_phy->mode = mode;
-
-	return 0;
-}
-
 static int mxs_phy_dcd_start(struct mxs_phy *mxs_phy)
 {
 	void __iomem *base = mxs_phy->phy.io_priv;
@@ -1096,7 +1087,6 @@ static int mxs_phy_probe(struct platform_device *pdev)
 	else
 		mxs_phy->phy.charger_detect	= mxs_phy_charger_detect;
 
-	mxs_phy->phy.set_mode		= mxs_phy_set_mode;
 	if (mxs_phy->data->flags & MXS_PHY_SENDING_SOF_TOO_FAST) {
 		mxs_phy->phy.notify_suspend = mxs_phy_on_suspend;
 		mxs_phy->phy.notify_resume = mxs_phy_on_resume;
